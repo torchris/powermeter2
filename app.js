4d06442e8da5e0563ec834fcc2e21db7ec76e277
GIT Admin <torchris4@gmail.com>
2014-03-01 17:56:43 -0500

    Commit of final changes on publication of my blog.
    Baseline for next stages.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+
+//Load Dependencies
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -12,14 +14,21 @@ var tungus = require('tungus');
 var Engine = require('tingodb');
 var mongoose = require('mongoose');
 var request = require("request");
+
+//Initialize Mongoose with Tingo
 var db = mongoose.connect('tingodb://readingsdb');
+
+//Set up some global variables
 var usage;
 var temp;
 var readings;
 
+//Set environment variables for defaults
 process.env['SAMPLES'] = '15';
 process.env['REFRESHINT'] = '120';
 
+
+//Mongoose connects to database
 mongoose.connect('tingodb://readingsdb', function (err){
     if (!err) {
         console.log('connected to databse');
@@ -28,43 +37,48 @@ mongoose.connect('tingodb://readingsdb', function (err){
     }
 });
 
+//Set up schema for database
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var ReadingsSchema = new Schema({
   time: { type : Date, default: Date.now },
-  usage2: Number,
-  temp2: Number
-});
+    usage2: Number,
+    temp2: Number
+    });
 
 var Readings = mongoose.model('Readings', ReadingsSchema);
 
+//Do the simple screen scrape and pop the results into the database.
+//Rinse and repeat according to the environment variable for refresh rate
 function getData (){
     request({uri: "http://192.168.1.33/pcmconfig.htm"}, function (error, response, body){
-    if (!error && response.statusCode == 200) {
-        var n = body.search("Present Demand");
-        usage = body.substr((n+44),5);
-        usage = usage.trim();
-        console.log(usage);
-        var n2 = body.search("Sensor Temp");
-        temp = body.substr((n2+40),3);
-        temp = temp.trim();
-        temp = temp - 2;
-        console.log(temp);
-        var readingInfo = new Readings({
-            temp2: temp,
-            usage2: usage
-        });
+        if (!error && response.statusCode == 200) {
+            var n = body.search("Present Demand");
+           usage = body.substr((n+44),5);
+           usage = usage.trim();
+           console.log(usage);
+           var n2 = body.search("Sensor Temp");
+           temp = body.substr((n2+40),3);
+           temp = temp.trim();
+           temp = temp - 2;
+           console.log(temp);
+           var readingInfo = new Readings({
+               temp2: temp,
+               usage2: usage
+            });
         readingInfo.save(function(err, readingInfo){
               if (err) return console.error(err);
             console.dir(readingInfo);
             });
         setTimeout(getData,(process.env.REFRESHINT * 1000));
-        }});
+    }});
 }
 
+//run the above function
 getData();
 
+//Below are all set by default in Express
 var app = express();
 
 // all environments
@@ -84,6 +98,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//When the Submit button is pressed, it runs this function and re-renders the page
 app.post('/', function(req, res){
     process.env['SAMPLES'] = req.param("samples");
     process.env['REFRESHINT'] = req.param("interval");
@@ -101,6 +116,7 @@ app.post('/', function(req, res){
           );
        });
 
+//This serves up the page and renders the variables into the Jade template engine.
 app.get('/', function(req, res){
   Readings.find({}, {}, { sort: { 'time' : -1}, limit: process.env.SAMPLES }, function(err, readings) {
     if (err) return console.error(err);
@@ -115,6 +131,7 @@ app.get('/', function(req, res){
        }
     );
 
+//Start up the server!
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
